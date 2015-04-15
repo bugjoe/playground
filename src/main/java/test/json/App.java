9a889f7f93822b8386058fc9fb7563a14ec6f56c
Josef Bauer <josef.bauer@gi-de.com>
2015-04-15 13:33:50 +0200

    Added 'object to JSON' functionallity

--- src/main/java/test/json/App.java
+++ src/main/java/test/json/App.java
@@ -3,11 +3,16 @@ package test.json;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.StringWriter;
 
 public class App {
     public static void main(String ... args) throws Exception {
         final ObjectMapper objectMapper = new ObjectMapper();
-        final Record record = objectMapper.readValue(new File("/home/bauerjos/projects/dns-test/target/classes/record.json"), Record.class);
+        final Record record = objectMapper.readValue(new File("record.json"), Record.class);
         System.out.println("Record POJO: " + record);
+        final StringWriter stringWriter = new StringWriter();
+        objectMapper.writeValue(stringWriter, record);
+        System.out.println("JSON String: " + stringWriter.toString());
+        stringWriter.close();
     }
 }
