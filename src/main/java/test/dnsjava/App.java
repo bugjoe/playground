3d2b13509b9002dbbb56aad3ccab5262b8cd58b8
Josef Bauer <josef.bauer@gi-de.com>
2015-04-20 13:44:37 +0200

    Moved code for zone extraction to own method

--- src/main/java/test/dnsjava/App.java
+++ src/main/java/test/dnsjava/App.java
@@ -9,11 +9,14 @@ public class App {
     public static void main(String ... args) throws Exception {
         final Name name = Name.fromString("foobar.host.com.");
         final Record record = Record.fromString(name, Type.A, DClass.IN, 3600, "123.123.123.123", null);
-        System.out.println("Record: " + record);
-        System.out.println("Additional Name: " + record.getAdditionalName());
-        System.out.println("Name:            " + record.getName());
+        System.out.println("Record          : " + record);
+        System.out.println("Additional Name : " + record.getAdditionalName());
+        System.out.println("Name            : " + record.getName());
+        System.out.println("Zone            : " + extractZoneName(name));
+    }
+
+    private static String extractZoneName(Name name) {
         final int indexFirstDot = name.toString().indexOf(".");
-        final String zone = name.toString().substring(indexFirstDot + 1);
-        System.out.println("Zone:            " + zone);
+        return name.toString().substring(indexFirstDot + 1);
     }
 }
