c41532d75a864183a1a3c8af9b1c1ca0d5794444
Josef Bauer <bauerjoe@gmx.de>
2015-05-10 18:34:33 +0200

    Improved Gulp scanning

--- src/main/java/htmlparser/App.java
+++ src/main/java/htmlparser/App.java
@@ -1,70 +1,28 @@
 package htmlparser;
 
-import com.google.common.io.Files;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.Map;
-
 public class App {
-	private static final String LEADING_ZEROES = "00000000";
-
 	public static void main(String... args) throws Exception {
-		final Map<String, Document> profiles = parse(1423777170, 1423777200);
 
-		for (String profileId : profiles.keySet()) {
-			final String fileName = "profiles/" + profileId + ".txt";
-
-			final File storeDir = new File("profiles");
-
-			if (!storeDir.mkdir()) {
-				System.out.printf("Directory (%s) was not created (maybe it already exists)%n", storeDir.toString());
-			}
-
-			final Document document = profiles.get(profileId);
-
-			Files.write(document.toString(), new File(fileName), Charset.defaultCharset());
-		}
-	}
-
-	private static Map<String, Document> parse(long start, long end) {
-		int validPorfiles = 0,
-			invalidProfiles = 0;
-
-		final Map<String, Document> profiles = new HashMap<String, Document>();
-
-		for (long i = start; i <= end; i++) {
-			final String profileId = getHexString(i);
-			final String url = "https://www.gulp.de/freiberufler/" + profileId + ".html";
-//			System.out.printf("Parsing URL: %s%n", url);
-
-			try {
-				final Document document = Jsoup.connect(url).get();
-				profiles.put(profileId, document);
-				System.out.printf("Profile ID valid:   %s%n", profileId);
-				validPorfiles++;
-			} catch (IOException e) {
-				System.out.printf("Profile ID invalid: %s%n", profileId);
-				invalidProfiles++;
-			}
+		if (args.length != 3 )
+		{
+			System.out.println("Please specify the following parameters:");
+			System.out.println("1.) number of parallel running jobs");
+			System.out.println("2.) number of first profile as hex");
+			System.out.println("3.) number of last profile as hex");
+			System.exit(1);
 		}
 
-		System.out.printf("Found %d valid profile(s) and %d invalid profile(s)", validPorfiles, invalidProfiles);
+		final int numberOfJobs = Integer.parseInt(args[0]);
+		final String foo = "0x0";// + args[1];
+		final long firstProfile = Long.decode(foo);
+		final String bar = "0xFF";// + args[2];
+		final long lastProfile = Long.decode(bar);
+		final long numberOfProfilesPerJob = (lastProfile - firstProfile) / numberOfJobs;
 
-		return profiles;
-	}
-
-	private static String getHexString(long number) {
-		final String hexString =  Long.toHexString(number).toUpperCase();
-
-		final int hexStringLength = hexString.length();
-
-		final String hexStringWithLeadingZeroes = LEADING_ZEROES + hexString;
-
-		return hexStringWithLeadingZeroes.substring(hexStringLength, hexStringWithLeadingZeroes.length());
+		for (long _firstProfile = firstProfile; _firstProfile < lastProfile; _firstProfile += numberOfProfilesPerJob)
+		{
+			final Scanner scanner = new Scanner(_firstProfile, firstProfile + numberOfProfilesPerJob);
+			new Thread(scanner).start();
+		}
 	}
 }
---
+++ src/main/java/htmlparser/Scanner.java
@@ -0,0 +1,96 @@
+package htmlparser;
+
+import com.google.common.io.Files;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Scanner implements Runnable
+{
+	private static final String LEADING_ZEROES = "00000000";
+	private final long startProfile;
+	private final long endProfile;
+
+	public Scanner(long startProfile, long endProfile)
+	{
+		this.startProfile = startProfile;
+		this.endProfile = endProfile;
+	}
+
+	public void run()
+	{
+
+		System.out.printf("Scanning from %s to %s %n",
+				Long.toHexString(startProfile).toUpperCase(),
+				Long.toHexString(endProfile).toUpperCase());
+
+		int validProfiles = 0;
+		int invalidProfiles = 0;
+
+		final Map<String, Document> profiles = new HashMap<String, Document>();
+
+		final long startTime = System.currentTimeMillis() / 1000;
+		for (long i = startProfile; i <= endProfile; i++)
+		{
+			final String profileId = getProfileHexString(i);
+			final String url = "https://www.gulp.de/freiberufler/" + profileId + ".html";
+
+			try
+			{
+				final Document document = Jsoup.connect(url).get();
+				profiles.put(profileId, document);
+				validProfiles++;
+			}
+			catch (IOException e)
+			{
+				invalidProfiles++;
+			}
+		}
+		final long endTime = System.currentTimeMillis() / 1000;
+
+		System.out.printf("Scanned %d profiles in %d seconds%n", endProfile - startProfile, endTime - startTime);
+		System.out.printf("Found %d valid profile(s) and %d invalid profile(s)%n", validProfiles, invalidProfiles);
+		System.out.printf("Writing found profiles to disc ... ");
+		writeToDisc(profiles);
+		System.out.printf("DONE%n%n");
+	}
+
+	private void writeToDisc(Map<String, Document> profiles)
+	{
+		for (String profileId : profiles.keySet()) {
+			final String fileName = "profiles/" + profileId + ".txt";
+
+			final File storeDir = new File("profiles");
+
+			if (!storeDir.mkdir()) {
+				System.out.printf("Directory (%s) was not created (maybe it already exists)%n", storeDir.toString());
+			}
+
+			final Document document = profiles.get(profileId);
+
+			try
+			{
+				Files.write(document.toString(), new File(fileName), Charset.defaultCharset());
+			}
+			catch (IOException e)
+			{
+				System.out.printf("ERROR (%s) while writing profile (%s) to disc%n", e.getMessage(), profileId);
+			}
+		}
+	}
+
+	private static String getProfileHexString(long number) {
+		final String hexString =  Long.toHexString(number).toUpperCase();
+
+		final int hexStringLength = hexString.length();
+
+		final String hexStringWithLeadingZeroes = LEADING_ZEROES + hexString;
+
+		return hexStringWithLeadingZeroes.substring(hexStringLength, hexStringWithLeadingZeroes.length());
+	}
+}
