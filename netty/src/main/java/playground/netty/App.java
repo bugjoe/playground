d4782aaf1d19449569ccb367089e006d3cc0e233
Josef Bauer <josef.bauer@webants.com>
2016-05-11 18:04:14 +0200

    Added gradle build file. Changed Netty UDP server to work like described here: http://stackoverflow.com/questions/37120249/netty-udp-performance-issue

--- netty/src/main/java/playground/netty/App.java
+++ netty/src/main/java/playground/netty/App.java
@@ -7,41 +7,37 @@ import io.netty.channel.socket.nio.NioDatagramChannel;
 
 public class App
 {
-	private static final EventLoopGroup EVENT_LOOP_GROUP = new NioEventLoopGroup();
+	private static EventLoopGroup EVENT_LOOP_GROUP;
 
 
 	public static void main(String[] args) throws Exception
 	{
-		if (args.length < 2)
+		if (args.length < 3)
 		{
 			System.out.println("Need following arguments");
 			System.out.println("1) port");
-			System.out.println("2) max. work time in ms");
+			System.out.println("2) number of worker threads");
+			System.out.println("3) max. work time in ms");
 			System.exit(0);
 		}
 
 		Runtime.getRuntime().addShutdownHook(new ShutdownActions());
 
 		final int port = Integer.parseInt(args[0]);
-		final int sleep = Integer.parseInt(args[1]);
+		final int threads = Integer.parseInt(args[1]);
+		final int sleep = Integer.parseInt(args[2]);
 
-		System.out.printf("Listening on port %d%n", port);
-		System.out.printf("Max. work time %d ms%n", sleep);
+		EVENT_LOOP_GROUP = new NioEventLoopGroup(threads);
+
+		System.out.printf("Listening on port:\t%d%n", port);
+		System.out.printf("Worker threads:   \t%d%n", threads);
+		System.out.printf("Max. work time:   \t%d ms%n", sleep);
 
 		final Bootstrap bootstrap = new Bootstrap();
 		bootstrap.group(EVENT_LOOP_GROUP);
 		bootstrap.channel(NioDatagramChannel.class);
 		bootstrap.handler(new MyNettyUdpHandler(sleep));
-//		bootstrap.bind(port).sync().channel().read().closeFuture().sync();
-		final NioDatagramChannel channel = (NioDatagramChannel)bootstrap.bind(port).sync().channel();
-
-		while (channel.isOpen())
-		{
-			channel.read();
-			System.out.println("aha");
-		}
-
-		channel.read().closeFuture().sync();
+		bootstrap.bind(port).sync().channel().read().closeFuture().sync();
 	}
 
 	private static final class ShutdownActions extends Thread
--- netty/src/main/java/playground/netty/MyNettyUdpHandler.java
+++ netty/src/main/java/playground/netty/MyNettyUdpHandler.java
@@ -1,50 +1,68 @@
 package playground.netty;
 
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.handler.codec.MessageToMessageDecoder;
 
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicLong;
-
 /**
  * @author josef.bauer@webants.com
  */
-public class MyNettyUdpHandler extends MessageToMessageDecoder<DatagramPacket> {
-    private static final AtomicLong COUNTER = new AtomicLong();
-    private final Random random = new Random(System.currentTimeMillis());
-    private final int sleep;
+public class MyNettyUdpHandler extends MessageToMessageDecoder<DatagramPacket>
+{
+	private static final AtomicLong COUNTER = new AtomicLong();
+	private final Random random = new Random(System.currentTimeMillis());
+	private final int sleep;
 
-    public MyNettyUdpHandler(int sleep) {
-        this.sleep = sleep;
-    }
+	public MyNettyUdpHandler(int sleep)
+	{
+		this.sleep = sleep;
+	}
 
-    @Override
-    protected void decode(ChannelHandlerContext channelHandlerContext, DatagramPacket datagramPacket, List list) throws Exception {
-        new Thread(() -> {
-            final long currentCount = COUNTER.incrementAndGet();
+	@Override
+	protected void decode(ChannelHandlerContext channelHandlerContext, DatagramPacket datagramPacket, List list) throws Exception
+	{
+//        new Thread(() -> {
+//            final long currentCount = COUNTER.incrementAndGet();
+//
+//            try {
+//                Thread.sleep(random.nextInt(sleep));
+//            } catch (InterruptedException e) {
+//                System.out.println("ERROR while sleeping");
+//            }
+//
+//            final ByteBuf buffer = Unpooled.buffer(64);
+//            buffer.writeBytes("Hey there".getBytes());
+//            channelHandlerContext.channel().writeAndFlush(new DatagramPacket(buffer, datagramPacket.sender()));
+//
+//            if (currentCount % 1000 == 0) {
+//                System.out.println(Thread.currentThread().toString() + " / " + currentCount);
+//            }
+//        }).start();
 
-            try {
-                Thread.sleep(random.nextInt(sleep));
-            } catch (InterruptedException e) {
-                System.out.println("ERROR while sleeping");
-            }
+		final Channel channel = channelHandlerContext.channel();
+		channel.eventLoop().schedule(() -> {
+			final ByteBuf buffer = Unpooled.buffer(64);
+			buffer.writeBytes("Hey there".getBytes());
+			channel.writeAndFlush(new DatagramPacket(buffer, datagramPacket.sender()));
+			final long currentCount = COUNTER.incrementAndGet();
+			if (currentCount % 1000 == 0)
+			{
+				System.out.println(Thread.currentThread().toString() + " / " + currentCount);
+			}
+		}, random.nextInt(sleep), TimeUnit.MILLISECONDS);
+	}
 
-            final ByteBuf buffer = Unpooled.buffer(64);
-            buffer.writeBytes("Hey there".getBytes());
-            channelHandlerContext.channel().writeAndFlush(new DatagramPacket(buffer, datagramPacket.sender()));
-
-            if (currentCount % 1000 == 0) {
-                System.out.println(Thread.currentThread().toString() + " / " + currentCount);
-            }
-        }).start();
-    }
-
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        System.out.printf("ERROR: %s%n", cause.getMessage());
-    }
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception
+	{
+		System.out.printf("ERROR: %s%n", cause.getMessage());
+	}
 }
